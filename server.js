919640b88a06f26b344bb465fbde4455c0dc7a19
Shelby Cloyd <shel@Shelbys-MacBook-Air.local>
2022-11-16 20:32:38 -0800

    start function with switches for future functions

--- server.js
+++ server.js
@@ -24,4 +24,39 @@ database.connect(function(err){
     start();
 });
 
-
+function start(){
+    inquirer
+    .prompt([
+        {
+            type: 'list',
+            message: 'Choose an option below'
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
+        }
+    ]).then(function(res) {
+        switch(res.start){
+            case 'View all departments':
+                viewDepartment();
+                break;
+            case 'View all roles':
+                viewRole();
+                break;
+            case 'View all employees':
+                viewEmployee();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployee();
+                break;
+            default:
+                console.log('default')
+        }
+    });
+};
