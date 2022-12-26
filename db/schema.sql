bd4967035e71be6fa989efd2e764b2c44350d8ae
Shel Cloyd <shel.cloyd@gmail.com>
2022-12-26 14:16:32 -0800

    changed character limit

--- db/schema.sql
+++ db/schema.sql
@@ -5,22 +5,22 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL
+    department_name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    job_title VARCHAR(30),
+    job_title VARCHAR(50),
     salary DECIMAL,
-    department_name VARCHAR(30)
+    department_name VARCHAR(50)
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    job_title VARCHAR(30),
-    department_name VARCHAR(30),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    job_title VARCHAR(50),
+    department_name VARCHAR(50),
     salary DECIMAL,
     manager_id INT
 );
